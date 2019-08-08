790f346a86b51a3877fb8c3fd299259a56191edc
Guilherme <guiheurich@gmail.com>
2019-08-08 20:44:14 +0100

    Added an extra test to scan method

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,13 +4,21 @@ describe Checkout do
 
   subject(:co) { described_class.new }
 
-  # co.scan("001")
   describe "#scan" do
     context 'when given an item' do
       it 'adds a reference to the item in the basket' do
         co.scan("001")
         expect(co.basket).to include("001")
       end
+    end
+  end
+
+  context 'when given multiple items' do
+    it 'contains all of them' do
+      co.scan("001")
+      co.scan("002")
+      co.scan("003")
+      expect(co.basket.length).to eq(3)
     end
   end
 end
