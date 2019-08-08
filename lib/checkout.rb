505b92b4a83cbdf39dcbcc430ec72052c7c37acb
Guilherme <guiheurich@gmail.com>
2019-08-08 20:52:01 +0100

    Implement total method
    When multiple items are in the basket, it returns the correct total price

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -4,10 +4,23 @@ class Checkout
 
   def initialize
     @basket = []
+    @products = {
+      "001" => { "Name" => "Very Cheap Chair", "Price" => 9.25 },
+      "002" => { "Name" => "Little Table", "Price" => 45.00 },
+      "003" => { "Name" => "Funky Light", "Price" => 19.95 }
+    }
   end
 
   def scan(item)
     @basket << item
+  end
+
+  def total
+    @total = 0
+    @basket.each do |item|
+      @total += @products[item]["Price"]
+    end
+    return @total
   end
 
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -11,14 +11,32 @@ describe Checkout do
         expect(co.basket).to include("001")
       end
     end
+
+    context 'when given multiple items' do
+      it 'contains all of them' do
+        co.scan("001")
+        co.scan("002")
+        co.scan("003")
+        expect(co.basket.length).to eq(3)
+      end
+    end
   end
 
-  context 'when given multiple items' do
-    it 'contains all of them' do
-      co.scan("001")
-      co.scan("002")
-      co.scan("003")
-      expect(co.basket.length).to eq(3)
+  # co.total
+  describe '#total' do
+    context 'when items are in the basket' do
+      it 'returns the total price' do
+        co.scan("001")
+        expect(co.total).to eq(9.25)
+      end
+    end
+
+    context 'when multiple items are in the basket' do
+      it 'returns the correct total price' do
+        co.scan("001")
+        co.scan("002")
+        expect(co.total).to eq(54.25)
+      end
     end
   end
 end
